0692115bc3a4d825caa8f145292eec9c035b90bc
byungju-oh <62101764+byungju-oh@users.noreply.github.com>
2020-06-01 14:57:47 +0900

    f

--- app.py
+++ app.py
@@ -12,14 +12,26 @@ from inference import get_type
 
 @app.route('/')
 def main():
-    return render_template('ex.html')
+    return render_template('base.html')
+
+
+@app.route('/e')
+def mn():
+    
+
+    return render_template('ext.html')
+@app.route('/s')
+def s():
+    
+
+    return render_template('s.html')
 
 
 @app.route('/h', methods=['GET','POST'])
 
 def h():
     if request.method=='GET':
-        return render_template('upload.html', value="ddi")
+        return render_template('up.html', value="ddi")
     if request.method=='POST':
         if 'canbe anything' not in request.files:
             print("file not uploaded")
@@ -27,9 +39,26 @@ def h():
         image = file.read()
         
         
-        category=get_type(image_bytes=image)        
+        category=get_type(image_bytes=image)
+        if category=='마른남자':        
     
-        return render_template('result.html', result=category)
+            return render_template('result.html', result=category)
+        
+        elif category=='남자비만':
+            return render_template('result1.html', result=category)
+        
+        elif category=='정상남자':
+            return render_template('result2.html', result=category)
+       
+        elif category=='마른여자':
+            return render_template('result3.html', result=category)
+        
+        elif category=='여자비만':
+            return render_template('result4.html', result=category)
+        
+        elif category=='정상여자':
+            return render_template('result5.html', result=category)
+
 
 
 if __name__ == "__main__":
